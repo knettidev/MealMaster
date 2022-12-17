2af16372b799ae5d5c1baf4affb6b172f45e4f7a
KazuoCode <KazuoEdits@gmail.com>
2022-12-17 21:26:00 +0100

    feat: Reformatted the button handle functions
    For more clarity now each button has it's own handle function, I'm
    mocking a request for debug purposes in getSearchResults

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import './App.css';
-import { Input, Button } from 'semantic-ui-react';
+import { Input, Button, Form } from 'semantic-ui-react';
 
-const getSearchResults = (endpoint) => {
-  fetch(`http://localhost:3001/${endpoint}`).then((res) => {
+const getSearchResults = (endpoint, data) => {
+  data = {
+    search: 'Pizza',
+    diet: 'Vegan',
+    allergen: ['Lactose'],
+    ingredient: ['Tomato'],
+  };
+
+  console.log(data);
+  fetch(`http://localhost:3001/${endpoint}`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((res) => {
     console.log(res.statusText);
   });
 };
 
 const searchElements = () => {
+  const getFormData = () => {
+    let searchBar = document.getElementById('searchBar').value;
+
+    return { searchBar };
+  };
+
+  const handleSearch = () => {
+    let data = getFormData();
+    if (data.searchBar === '') {
+      let form = document.getElementsByTagName('form')[0];
+      form.reportValidity();
+      return;
+    }
+    getSearchResults('search', data);
+  };
+
+  const handleSurprise = () => {
+    let data = getFormData();
+    getSearchResults('surprise', data);
+  };
+
   return (
     <div id="searchFields">
-      <Input id="searchBar" type="text" placeholder="Search..." />
-      <div id="searchButtons">
-        <Button
-          type="button"
-          onClick={() => getSearchResults('search')}
-          primary
-        >
-          Search
-        </Button>
-        <Button type="button" onClick={() => getSearchResults('surprise')}>
-          Surprise me!
-        </Button>
-      </div>
+      <Form>
+        <Input id="searchBar" type="text" placeholder="Search..." required />
+        <div id="searchButtons">
+          <Button type="button" onClick={handleSearch} primary>
+            Search
+          </Button>
+          <Button type="button" onClick={handleSurprise}>
+            Surprise me!
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
